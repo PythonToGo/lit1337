ced4b1147ce3e054d0194d04e4340458967bee62
PythonToGo <pythontogoplease@gmail.com>
2025-04-04 21:10:55 +0200

    try: add flush=True in init_db

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,17 +1,20 @@
 from database import engine
 from models import Base
 import asyncio
+import sys
+import traceback
 
 async def init_db():
-    print("🟡 [init_db] Trying to initialize DB...")
+    print("🟡 [init_db] Trying to initialize DB...", flush=True)
 
     try:
         async with engine.begin() as conn:
-            print("🟡 [init_db] Connected to DB, creating tables...")
+            print("🟡 [init_db] Connected to DB, creating tables...", flush=True)
             await conn.run_sync(Base.metadata.create_all)
-            print("🟢 [init_db] DB tables created successfully.")
+            print("🟢 [init_db] DB tables created successfully.", flush=True)
     except Exception as e:
-        print("🔴 [init_db] DB Initialization failed:", e)
+        print("🔴 [init_db] DB Initialization failed:", e, flush=True)
+        traceback.print_exc(file=sys.stdout) 
 
 # local test
 if __name__ == "__main__":

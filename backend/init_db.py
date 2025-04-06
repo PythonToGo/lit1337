6c2c1018bfcbc11659c9e737f2e5a2dc0e4a5952
PythonToGo <pythontogoplease@gmail.com>
2025-04-06 10:39:10 +0200

    deploy fix 22a3957: run alembic migration outside async context

--- backend/init_db.py
+++ backend/init_db.py
@@ -5,27 +5,27 @@ from alembic import command
 from dotenv import load_dotenv
 
 load_dotenv()
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def init_db():
+def run_migrations():
     logger.info("🟡 [init_db] Starting Alembic DB migration...")
 
     try:
-        # alembic settings
         alembic_cfg = Config("alembic.ini")
         db_url = os.getenv("DATABASE_URL")
         if not db_url:
             raise ValueError("DATABASE_URL not found in environment variables.")
-
         alembic_cfg.set_main_option("sqlalchemy.url", db_url)
 
         command.upgrade(alembic_cfg, "head")
         logger.info("🟢 [init_db] Alembic migration applied successfully.")
-        
     except Exception as e:
         logger.exception("🔴 [init_db] Alembic migration failed")
+
+# async 함수에서 동기 함수로 분리
+async def init_db():
+    run_migrations()
 
 
 # local test
